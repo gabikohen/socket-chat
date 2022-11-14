c3583a8703452dbd2a59f3bef170365860b3b852
gabikohen <gabokohen@gmail.com>
2022-11-14 17:39:00 -0300

    socket.io

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    /* console.log('Un usuario se conecto'); */
+
+    /*  socket.on('chat',(msg)=>{
+       io.emit('chat',msg)
+     }) */
+
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg)
+    })
+})
+
+app.get('/', (req, res) => {
+
+    res.sendFile(`${__dirname}/cliente/index.html`)
+})
+server.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000')
+});
